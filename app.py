beb4bce200a9377852e2b6d65f7998b31d7c53c5
parsibharadwaj <83240599+parsibharadwaj@users.noreply.github.com>
2022-02-26 01:59:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+from flask import render_template, request
+from flask_cors import CORS
+
+app = flask.Flask(__name__,)
+CORS(app)
+
+@app.route('/',methods=['GET'])
+def default():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #extract data from post request
+    X = float(request.form['A'])
+    Y = float(request.form['B'])
+    Z = float(request.form['C'])
+    K = float(request.form['D'])
+    average=(((X*Y)+(Z*K))/Y+K)
+    
+    return render_template('predict.html', average=average, sep=K)
+
+if __name__ == '__main__':
+    app.run()
